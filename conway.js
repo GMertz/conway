e7e369e109cd9f2c9448f0be79bd0fc7d3d30a86
Gabe Mertz <gabe.mertz@crms.org>
2018-11-14 14:17:59 +0800

    cleaned up stuff, optimized code

--- conway.js
+++ conway.js
@@ -1,184 +1,161 @@
 "use strict";
-var board;
-var rows = 20;
-var box;
-var ctx;
-var s = 5;
-var mouseX= 0,mouseY=0;
-var mDown=false;
-var update;
-var playing = false;
-var offSets = [{x:0,y:-1},{x:1,y:-1},{x:1,y:0},{x:1,y:1},{x:0,y:1},{x:-1,y:1},{x:-1,y:0},{x:-1,y:-1}];
-var cellCol ={on:"white",off:"black"};
-var borderCol = "lightblue";
-var rowCount;
-var playButton;
-/*
-weird box spawned at 0,0 for somereason
-fucked performance, garbade collection maybe 
-let values wrap, add some iffys
-*/
+var offsets = [{x:0,y:-1},{x:1,y:-1},{x:1,y:0},{x:1,y:1},{x:0,y:1},{x:-1,y:1},{x:-1,y:0},{x:-1,y:-1}];
 
 window.onload = function(){
-	board = document.getElementById("game");
-	board.addEventListener("mousedown",mouseDown);
-	document.addEventListener("mouseup",mouseUp);
-	board.addEventListener("mousemove",mousePos);
- 	playButton = document.getElementById("play");
+	var gameBoard = new Board();
+	var playing = false;
+	var gameInterval;
+	var mDown = false;
+	var lastClick = {x:-1,y:-1};
+	var MMVal = 1;
+
+	var canvas = document.getElementById("game");
+	canvas.width = 500;
+	canvas.height = 500;
+	gameBoard.init(10,10,canvas);
+
+	canvas.addEventListener("mousedown", function(e){
+		mDown = true;
+		var x = Math.floor((e.pageX - canvas.offsetLeft)/(canvas.width/gameBoard.cols));
+		var y = Math.floor((e.pageY - canvas.offsetTop)/(canvas.height/gameBoard.rows));
+		if(x > -1 && x < gameBoard.cols && y > -1 && y < gameBoard.rows){
+			var val = gameBoard.cells[x][y]? 0:1;
+			gameBoard.cells[x][y] = val;
+			gameBoard.drawCell(x,y,val);
+			lastClick = {x:x,y:y};
+			MMVal = val;
+		}
+	});
+
+	document.addEventListener("mouseup",function(){ mDown = false});
+	
+	canvas.addEventListener("mousemove",function(e){
+		if(mDown){
+			var x = Math.floor((e.pageX - canvas.offsetLeft)/(canvas.width/gameBoard.cols));
+			var y = Math.floor((e.pageY - canvas.offsetTop)/(canvas.height/gameBoard.rows));
+			if(lastClick.x == x && lastClick.y == y)return;
+			if(x > -1 && x < gameBoard.cols && y > -1 && y < gameBoard.rows){
+				gameBoard.cells[x][y] = MMVal;
+				gameBoard.drawCell(x,y,MMVal);
+			}
+		}	
+	});
+	var playButton = document.getElementById("play");
 	playButton.addEventListener("click",function(){
-		if(playing == false){
-			playButton.innerHTML = "Pause";
-			update = setInterval(tick,1000/10);
-			playing = true;
-		}else
-			pause();
-	});
-	document.getElementById("reset").addEventListener("click",function(){
-		if(playing == false){
-		console.log("working");
-			for(var i = 0; i<box.rows; i++){
-					for(var k = 0; k<box.rows;k++){
-						var c = box.cells[i][k];
-						c.alive = true;
-						c.draw();
+
+		playing = !playing;
+		playButton.innerHTML = playing? "Pause" : "Play";
+		if(playing)
+			gameInterval = setInterval(
+				function()
+				{
+					if(gameBoard.tick())
+					{
+						playButton.innerHTML = "Play";
+						playing = false;
+						clearInterval(gameInterval);
 					}
-			}
-		}
-		tick();
+				},1000/10);
+		else
+			clearInterval(gameInterval);
 	});
-	rowCount = document.getElementById("rows");
-	document.getElementById("init").addEventListener("click",initBoard);
-	board.width = 600;
-	board.height = 600;
-	ctx=board.getContext("2d");
-	initBoard();
-}
-function initBoard(){
-	rows = rowCount.value;
-	if(rows > 100)
-		rows = 100;
-	box = {cells:[],x:0,y:0,width:600,rows:rows};
-	for(var i = 0; i<box.rows; i++){
-		var row = [];
-		for(var k = 0; k<box.rows;k++){
-			var c = new cell(i,k,box.width/box.rows,false);
-			row.push(c);
-			c.draw();
-		}
-		box.cells.push(row);
-	}
-	console.log("pretick");
-	tick();
-	console.log("done");
-}
-function sketch(x,y,w,alive){
-	ctx.fillStyle = cellCol.off;
-	if(alive){
-		ctx.fillStyle = cellCol.on;
-	}
-	ctx.fillRect(x*w,y*w,w,w);
-	ctx.beginPath();
-	ctx.strokeStyle = borderCol;
-	ctx.lineWidth = 2;
-	ctx.rect(x*w,y*w,w,w);
-	ctx.stroke();
-}
-function cell (x,y,w,a){
-	this.x = x;
-	this.y = y;
-	this.w = w;
-	this.alive = a;
-	this.c = false;
-	//this.path = [{x:(x*hw)-hw,y:(y*hw)-hw},{x:(x*hw)+hw,y:(y*hw)-hw},{x:(x*hw)+hw,y:(y*hw)+hw},{x:(x*hw)-hw,y:(y*hw)+hw}];
-	this.n = function(){
-		var s = 0;
-		for(var i = 0; i<8; i++){
-			var x = offSets[i].x+this.x;
-			var y = offSets[i].y+this.y;
-			if(x < 0)
-				x = rows-1;
-			if(x >= rows)
-				x = 0;
-			if(y < 0)
-				y = rows-1;
-			if(y >= rows)
-				y = 0;
-			var sq = box.cells[x][y];
-			if(sq.alive)
-				s++;
-		}
-		if((this.alive) && (s < 2 || s > 3))
-			this.c = true;
-		else if(s == 3 && !this.alive){
-			this.c = true;
-		}
-	}
-	this.draw = function(){
-		sketch(this.x,this.y,this.w,this.alive);
-	}
-	this.change = function(){
-		if(this.c){
-			this.alive = !this.alive;
-			this.draw();
-			this.c = false;
-			return 1;
-		}
-		return 0;
 
-	}
-}
-function tick(){
-	for(var i = 0; i<rows; i++){
-		for(var k = 0; k<rows; k++){
-			var b = box.cells[i][k];
-			b.n();
-		}
-	}
-	var total = 0;
-	for(var i = 0; i<rows; i++){
-		for(var k = 0; k<rows; k++){
-			var b = box.cells[i][k];
-			total += b.change();
-		}
-	}
-	if(total < 1){
-		pause();
-	}
+	document.getElementById("reset").addEventListener("click",function(){
+		var cols = parseInt(document.getElementById("cols").value);
+		var rows = parseInt(document.getElementById("rows").value);
+		if (cols > 999) cols=1000;
+		if(rows > 999) rows=1000;
+		playing = false;
+		playButton.innerHTML = "Play";
+		gameBoard = new Board();
+		gameBoard.init(cols,rows,canvas);
+		console.log("Board Reset!");
+	});
 
+	var rowCount = document.getElementById("rows");
+	var colCount;
 }
-function mouseDown(){
-	mDown = true;
-	if(playing == false){
-			var x = Math.floor((mouseX)/(box.width/rows));
-			var y = Math.floor((mouseY)/(box.width/rows));
-			if(x > -1 && x < rows && y > -1 && y < rows){
-				var b = box.cells[x][y];
-				b.alive = !b.alive;
-				b.draw();
+
+function Board(){	
+	this.init = function(cols,rows,canvas,colors = ["black","white","white"])
+	{
+		this.cols = cols;
+		this.rows = rows;
+		this.w = canvas.width/cols;
+		this.h = canvas.height/rows;
+		this.canvas = canvas;
+		canvas.getContext("2d").fillRect(0,0,canvas.width,canvas.height,"white");
+		this.colors = colors;
+		this.borderWidth = (canvas.width+canvas.height)/(15*(cols+rows));
+		var cells = []
+		for (var i = 0; i < this.cols; i++) 
+		{
+			var row = [];
+			for (var k = 0; k < this.rows; k++) 
+			{
+				row.push(0);
+				this.drawCell(i,k,0);
 			}
-	}
-}
-function mouseUp(){
-		mDown = false;
-}
-function mousePos(e){
-		mouseX = e.pageX - board.offsetLeft;
-		mouseY = e.pageY - board.offsetTop;
-	if(playing == false && mDown){
-		if(mouseX > box.x && mouseX < box.x+box.width && mouseY > box.y && mouseY < box.y+box.width ){
-		var x = Math.floor((mouseX)/(box.width/rows));
-		var y = Math.floor((mouseY)/(box.width/rows));
-		if(x > -1 && x < rows && y > -1 && y < rows){
-			var b = box.cells[x][y];
-			b.alive = true;
-			b.draw();
+			cells.push(row);
 		}
+		this.cells = cells;
 	}
+	this.tick = function()
+	{
+		var flag = 1;
+		var state = Array(this.rows);
+		for(var i = 0; i < this.cols; i++){
+			state[i]=(Array(this.rows));
+			for(var k = 0; k < this.rows; k++){
+				state[i][k]=this.cells[i][k];
+			}
+		}
 		
-}
-}
-function pause(){
-	playButton.innerHTML = "Play";
-	clearInterval(update);
-	playing=false;
+		for (var i = 0; i < this.cols; i++) 
+		{
+			for (var k = 0; k < this.rows; k++) 
+			{				
+				var n = this.nNeighbors(i,k,state);
+				
+				if(state[i][k] && (n < 2 || n > 3))
+					this.cells[i][k] = 0;
+				else if(!state[i][k] && n == 3)
+					this.cells[i][k] = 1;
+
+				if(this.cells[i][k] != state[i][k]){
+					this.drawCell(i,k,this.cells[i][k]);
+					flag = 0;
+				}
+			}
+		}
+		return flag;
+	}
+
+	this.nNeighbors = function(x,y,state)
+	{
+		var n = 0;
+		for (var i = 0; i < 8; i++)
+		{
+			var oX = x+offsets[i].x, oY = y+offsets[i].y;
+
+			if(oX > -1 && oX < this.cols && oY > -1 && oY < this.rows)
+				if(state[oX][oY] == 1){
+					n++;
+				}
+		}	
+		return n;	
+	}
+
+	this.drawCell = function(x,y,val)
+	{
+		var ctx = this.canvas.getContext("2d");
+		ctx.fillStyle = this.colors[val];
+		ctx.fillRect(x*this.w,y*this.h,this.w-this.borderWidth/4,this.h-this.borderWidth/4);
+		ctx.beginPath();
+		ctx.strokeStyle = this.colors[2];
+		ctx.lineWidth = this.borderWidth;
+		ctx.rect(x*this.w,y*this.h,this.w,this.h);
+		ctx.stroke();
+	}
+
 }
